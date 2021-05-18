30493640fd66399c6d93365c5376b99a1e5757e4
mandeep111 <mandeepdhakal11@gmail.com>
2021-05-18 06:38:50 -0500

    user note service created

--- notes_microservice/src/main/java/com/microservice/notes_microservice/service/UserNoteServiceImpl.java
+++ notes_microservice/src/main/java/com/microservice/notes_microservice/service/UserNoteServiceImpl.java
@@ -1,4 +1,22 @@
 package com.microservice.notes_microservice.service;
 
-public class UserNoteServiceImpl {
+import com.microservice.notes_microservice.entity.UserNote;
+
+import java.util.List;
+
+public class UserNoteServiceImpl implements UserNoteService{
+    @Override
+    public List<UserNote> getNotes(String email) {
+        return null;
+    }
+
+    @Override
+    public void deleteNote(int id) {
+
+    }
+
+    @Override
+    public UserNote addNote(UserNote note) {
+        return null;
+    }
 }
