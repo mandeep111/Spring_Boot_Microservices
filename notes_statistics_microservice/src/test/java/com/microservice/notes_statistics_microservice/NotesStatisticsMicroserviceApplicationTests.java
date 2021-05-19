d0c64c4e2bbd96c737294da6c8d1369c93e253d8
mandeep111 <mandeepdhakal11@gmail.com>
2021-05-18 20:14:39 -0500

    notes statistics setup

---
+++ notes_statistics_microservice/src/test/java/com/microservice/notes_statistics_microservice/NotesStatisticsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.notes_statistics_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesStatisticsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
